ee3e77d4a571086a39eeb0fccae934567ea69760
abdullahhagras <93441365+abdullahhagras@users.noreply.github.com>
2024-01-11 18:30:06 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php include "layouts/header.php"; ?>
+<div class="container">
+
+<center><h2 style="color:black; margin-top:20%">Welcome to TomodachiChat!</h2>
+<h2 style="color:black; margin-top:20%">友達チャットへようこそ!</h2></center>
+
+</div>
+
+</body>
+</html>
+
+
